2b9d61ad1bcf24cec2dfff499f1af7253a4eb159
Shadman Hoque <shadman@Shadmans-iMac.local>
2020-12-19 22:03:56 -0500

    final commit

---
+++ src/main/java/com/peoplentech/seleniumpractice/SeleniumWorks.java
@@ -0,0 +1,10 @@
+package com.peoplentech.seleniumpractice;
+
+public class SeleniumWorks {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver");
+       // WebDriver webDriver = new ChromeDriver();
+    }
+}
+
